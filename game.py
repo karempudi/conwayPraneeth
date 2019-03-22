7bffdfcd40ab9b07b8587671dc3aacabb440baeb
Praneeth Karempudi <praneeth.karempudi@gmail.com>
2019-03-22 11:05:40 +0100

    Structured basic initialization into functions

--- game.py
+++ game.py
@@ -1,14 +1,18 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-# the size of the screen
-displaySize= 32
+def initialize_random(sizeOfGrid):
+    """ Function for randomly initializing a grid of particular size """
+    mat = np.random.randint(2, size = (sizeOfGrid, sizeOfGrid), dtype=bool)
+    
+    return mat
 
-# Doing random initializations
-mat = np.random.randint(2, size = (displaySize, displaySize), dtype=bool)
+def plotGameState(mat):
+    """ Function to call plotting of the game statue using matplot lib """
+    plt.imshow(mat)
+    plt.show()
 
-# Display the matrix using matplotlib
-plt.imshow(mat)
-plt.show()
+def calculateNeighbours(mat):
+    """ Function to calculate the neighbour matrix of the central grid, excluding the border of the grid"""
 
-
+    return None
